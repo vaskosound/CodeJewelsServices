16e1ba24a8ac3b79c7baa7eed4b4a809136885e8
Vasil Lazarov <vaskosound@abv.bg>
2013-08-17 11:35:00 -0700

    Add project.

---
+++ CodeJewels.Services/Controllers/CodeJewelsController.cs
@@ -0,0 +1,123 @@
+﻿using CodeJewels.DataLayer;
+using CodeJewels.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CodeJewels.Services.Controllers
+{
+    public class CodeJewelsController : ApiController
+    {
+        private DbContext context;
+        private DbSet<CodeJewel> entitySet;
+
+        public CodeJewelsController()
+        {
+            this.context = new CodeJewelsDBEntities();
+            this.entitySet = context.Set<CodeJewel>();
+        }
+
+        // GET api/codejewels
+        public IEnumerable<CodeJewelsModel> Get()
+        {
+            var codeJewels = from code in this.entitySet
+                             select new CodeJewelsModel()
+                             {
+                                 Id = code.Id,
+                                 SourceCode = code.SourceCode,
+                                 AuthorEmail = code.AuthorEmail,
+                                 Rating = code.Rating,
+                                 CodeLanguage = code.Category.CodeLanguage
+                             };
+            return codeJewels;
+        }
+
+        // GET api/codejewels/5
+        [ActionName("get")]
+        public IEnumerable<CodeJewelsModel> GetBySourceAndCategory(string source, string category)
+        {
+            var codeJewels = entitySet.Where(x => x.SourceCode.Contains(source) &&
+                x.Category.CodeLanguage.ToLower() == category.ToLower())
+                    .Select(new Func<CodeJewel, CodeJewelsModel>(x => new CodeJewelsModel()
+                    {
+                        Id = x.Id,
+                        SourceCode = x.SourceCode,
+                        AuthorEmail = x.AuthorEmail,
+                        Rating = x.Rating,
+                        CodeLanguage = x.Category.CodeLanguage
+                    })).ToList();
+
+            return codeJewels;
+        }
+
+        // POST api/codejewels
+        public HttpResponseMessage Post([FromBody]CodeJewelsModel value)
+        {
+            var codeJewel = new CodeJewel()
+            {
+                SourceCode = value.SourceCode,
+                AuthorEmail = value.AuthorEmail,
+                Rating = value.Rating
+            };
+
+            if (value.CodeLanguage != null)
+            {
+                var category = new Category();
+                category.CodeLanguage = value.CodeLanguage;
+                category.CodeJewels.Add(codeJewel);
+                var entytiCategories = context.Set<Category>();
+                entytiCategories.Add(category);
+            }
+            else
+            {
+                this.entitySet.Add(codeJewel);
+            }
+            this.context.SaveChanges();
+            CodeJewelsModel result = new CodeJewelsModel()
+            {
+                Id = codeJewel.Id,
+                SourceCode = codeJewel.SourceCode,
+                AuthorEmail = codeJewel.AuthorEmail,
+                Rating = codeJewel.Rating,
+                CodeLanguage = value.CodeLanguage
+            };
+
+            var response = Request.CreateResponse(HttpStatusCode.Created, result);
+            return response;
+        }
+
+        // PUT api/codejewels/5
+        public void Put(int id, string vote)
+        {
+            var codeJewel = entitySet.FirstOrDefault(x => x.Id == id);
+            if (vote == "plus")
+            {
+                codeJewel.Rating++;
+            }
+            else if (vote == "minus")
+            {
+                codeJewel.Rating--;
+            }
+            
+            context.SaveChanges();
+        }
+
+        // DELETE api/codejewels/5
+        [ActionName("delete")]
+        public void DeleteLowScores()
+        {
+            var minRating = entitySet.Min(x => x.Rating);
+            var codeJewelsMinRating = entitySet.Where(c => c.Rating == minRating);
+            foreach (var min in codeJewelsMinRating)
+            {
+                entitySet.Remove(min);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ CodeJewels.Services/Models/CodeJewelsModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeJewels.Services.Models
+{
+    public class CodeJewelsModel
+    {
+        public int Id { get; set; }
+
+        public string SourceCode { get; set; }
+
+        public int Rating { get; set; }
+
+        public string AuthorEmail { get; set; }
+
+        public string CodeLanguage { get; set; }
+    }
+}
